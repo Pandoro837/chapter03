2a1ba2ad1187f741ec9eafbd26e308be0e90b115
young <mould837@naver.com>
2021-06-11 15:55:47 +0900

    Object 클래스

--- src/com/javaex/ex02/PointApp.java
+++ src/com/javaex/ex02/PointApp.java
@@ -9,6 +9,30 @@ public class PointApp {
 		Point p02 = new Point(5,9);
 		Point p03 = p00;
 		
+//		getClass() 클래스 정보
+		System.out.println(".getClass()===========================");
+		System.out.println(p00.getClass());
+		System.out.println(p01.getClass());
+		System.out.println(p02.getClass());
+		System.out.println(p03.getClass());
+		System.out.println("======================================");
+
+//		hashCode() 주소에 대응하는 값
+		System.out.println(".hashCode()===========================");
+		System.out.println(p00.hashCode());
+		System.out.println(p01.hashCode());
+		System.out.println(p02.hashCode());
+		System.out.println(p03.hashCode());
+		System.out.println("======================================");
+
+//		toString() 객체의 정보
+		System.out.println(".toString()===========================");
+		System.out.println(p00.toString());
+		System.out.println(p01.toString());
+		System.out.println(p02.toString());
+		System.out.println(p03.toString());
+		System.out.println("======================================");
+
 //		equals() 객체가 같은지 비교
 		System.out.println(".equals()===========================");
 		System.out.println(p00.equals(p01)); //단순히 객체에 든 값이 같다고
